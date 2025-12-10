f053dd1929fd9ba2e6510cb15000ae2707ba59ca
juandavidcastrillonzapata <juandavidcastrillonzapata8@gmail.com>
2025-12-09 20:24:34 -0500

    mi cv julian robles

--- src/components/CabeceraCV.jsx
+++ src/components/CabeceraCV.jsx
@@ -1,9 +1,9 @@
 export default function CabeceraCV() {
     return (
     <header>
-        <h1>Juan David Castrillón Zapata</h1>
+        <h1>Julian robles</h1>
         <h2>Desarrollador Full Stack</h2>
-        <p>📧 juandavid@example.com | 📞 +57 300 000 0000</p>
+        <p>📧 elrobles@gmail.com | 📞 +57 300 000 0000</p>
     </header>
 );
 }
